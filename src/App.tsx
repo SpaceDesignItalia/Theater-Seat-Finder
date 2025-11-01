96d861e6f23dd5a83e2bac73aad4411b9882060e
Marco Macherelli <elmache05@gmail.com>
2025-11-01 15:20:03 +0100

    Select

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Select, SelectItem } from "@heroui/react";
 import { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
+  const [selected, setSelected] = useState<string>("Platea");
+
+  console.log(selected);
   return (
-    <Routes>
-      <Route path="/" />
-    </Routes>
+    <div>
+      <Select
+        selectedKeys={[selected]}
+        onSelectionChange={(keys) => setSelected(keys.currentKey as string)}
+      >
+        <SelectItem key="Platea">Platea</SelectItem>
+        <SelectItem key="Galleria">Galleria</SelectItem>
+        <SelectItem key="Palco">Palco</SelectItem>
+      </Select>
+    </div>
   );
 }
 
